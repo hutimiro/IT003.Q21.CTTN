336107e45b2f8819a96dd03486ac2d87ea30aa79
Quách Cường <128879616+hutimiro@users.noreply.github.com>
2026-02-02 07:07:57 +0700

    Refactor heapsort implementation and comments
    Refactor heapsort function and clean up comments.

--- heapsort.py
+++ heapsort.py
@@ -7,33 +7,27 @@ avgtime = 0
 def heapsort(arr):
     n = len(arr)
 
-    # Build a maxheap
     for i in range(n // 2 - 1, -1, -1):
         heapify(arr, n, i)
 
-    # One by one extract elements from heap
     for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # swap
+        arr[i], arr[0] = arr[0], arr[i]  
         heapify(arr, i, 0)
     return arr
 def heapify(arr, n, i):
-    largest = i  # Initialize largest as root
+    largest = i  
     left = 2 * i + 1     # left = 2*i + 1
     right = 2 * i + 2    # right = 2*i + 2
 
-    # See if left child of root exists and is greater than root
     if left < n and arr[i] < arr[left]:
         largest = left
 
-    # See if right child of root exists and is greater than root
     if right < n and arr[largest] < arr[right]:
         largest = right
 
-    # Change root, if needed
     if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+        arr[i], arr[largest] = arr[largest], arr[i] 
 
-        # Heapify the root.
         heapify(arr, n, largest)
 df = pd.read_csv('random_numbers.csv')
 for col in df.columns:
