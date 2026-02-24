3bd0c144e51e8a156b80148830818f04dc4d93ce
Quách Cường <128879616+hutimiro@users.noreply.github.com>
2026-02-24 22:06:12 +0700

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,15 +1,19 @@
 import numpy as np
 import pandas as pd
 import time
+from numba import njit
+
 avgtime = 0
+
+@njit
 def merge_sort(arr):
     if len(arr) > 1:
-        mid = len(arr) // 2 
-        L = arr[:mid]        
-        R = arr[mid:]
+        mid = len(arr) // 2
+        L = arr[:mid].copy()
+        R = arr[mid:].copy()
 
-        merge_sort(L)        
-        merge_sort(R)       
+        merge_sort(L)
+        merge_sort(R)
 
         i = j = k = 0
 
@@ -32,11 +36,13 @@ def merge_sort(arr):
             j += 1
             k += 1
     return arr
+
 df = pd.read_csv('random_numbers.csv')
 for col in df.columns:
     print(f"Sorting column: {col}")
     start_time = time.time()
-    sorted_integers = merge_sort(df[col].tolist())
+    arr = df[col].values.copy()  
+    sorted_integers = merge_sort(arr)
     end_time = time.time()
     avgtime = avgtime + (end_time - start_time)
     print(f"time taken to sort {col} using Mergesort: {(end_time - start_time) * 1000:.0f} ms") 
